f05e35eab5403192dd52a3f6a5804b507d6d98b9
meekbronsen <mikolskeezy@gmail.com>
2024-01-23 14:00:04 +0300

    Initial commit.

---
+++ backend/tempCodeRunnerFile.ts
@@ -0,0 +1,6 @@
+const storeItems = new Map([
+    [1,{price:10000, name: 'Learn React'}],
+    [2,{price: 20000, name: 'Learn CSS'}]
+])
+
+console.log(storeItems.values())
---
+++ frontend/src/main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContextProvider } from "./contexts/AppContextProvider.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
+import { SearchContextProvider } from "./components/SearchBar/SearchContextProvider.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppContextProvider>
+        <SearchContextProvider>
+          <RouterProvider router={router} />
+        </SearchContextProvider>
+      </AppContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
