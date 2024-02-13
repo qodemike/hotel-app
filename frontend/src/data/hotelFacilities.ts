3c4d1133260f8b29f27ffcd6c76d3c4d3b9f1f08
meekbronsen <mikolskeezy@gmail.com>
2024-02-13 21:02:07 +0300

    Remove data folder from .gitignore.

---
+++ frontend/src/data/hotelFacilities.ts
@@ -0,0 +1,11 @@
+
+export const hotelFacilities: string[] = [
+  "Free WiFi",
+  "Parking",
+  "Airport Shuttle",
+  "Family Rooms",
+  "No Smoking",
+  "Outdoor Pool",
+  "Spa",
+  "Fitness Center",
+];
