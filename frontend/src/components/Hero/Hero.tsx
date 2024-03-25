aeb50306127b26d401652a2c39877b6b8fc479ba
meekbronsen <mikolskeezy@gmail.com>
2024-03-25 13:33:34 +0300

    Add create hotel button on Hero section.

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,10 +1,23 @@
-import Button from "../Button";
+import { Button } from "@/components/ui/button";
 import Hero_Image_Scale_1217 from "../../assets/Hero_Image_scale,w_1217.png";
 import Hero_Image_Scale_1018 from "../../assets/Hero_Image_scale,w_1018.png";
 import Hero_Image_Scale_726 from "../../assets/Hero_Image_scale,w_726.png";
 import Hero_Image_Scale_300 from "../../assets/Hero_Image_scale,w_300.png";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../../contexts/Auth/AuthContext";
+import { toast } from "@/components/ui/use-toast";
 
 const Hero = () => {
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    if (!user)
+      toast({
+        variant: "destructive",
+        title: "Sign In first!",
+        description: "Sign in In first to create a new hotel!",
+      });
+  };
   return (
     <section className="h-fit lg:h-[calc(100vh-100px)] mx-6 md:mx-10 lg:mx-16 grid  grid-cols-1 lg:grid-cols-[1fr_1fr] items-center lg:gap-5">
       <div className=" mt-24 lg:mt-0 space-y-8 ">
@@ -15,8 +28,14 @@ const Hero = () => {
           Welcome to our hotel booking platform, where your travel experience
           becomes easier and enjoyable.
         </p>
-        <Button>Explore Hotels</Button>
-        <button className="bg-primary p"></button>
+        <div className="flex gap-4">
+          <Button className="rounded-none px-5">Explore Hotels</Button>
+          <Link to={"/add-hotel"} onClick={handleClick}>
+            <Button variant={"ghost"} className="rounded-none px-5">
+              Become a Hotelier
+            </Button>
+          </Link>
+        </div>
       </div>
 
       <picture className="h-fit justify-self-center">
--- frontend/src/components/HowItWorks/HowItWorksSection.tsx
+++ frontend/src/components/HowItWorks/HowItWorksSection.tsx
@@ -20,16 +20,16 @@ const HowItWorksSection = () => {
     {
       icon: "",
       step: " You're all Set!",
-      description: "After payment is successfull your room is secured ",
+      description: "After successful payment your room is secured ",
     },
   ];
 
   return (
     <section className=" py-10 pb-20 px-5 md:px-8 lg:px-16 bg-accentBg">
-      <div className="  text-white flex flex-col  items-center gap-3 ">
+      <div className="  text-white flex flex-col  items-center gap-2 ">
         <h2 className="font-bold text-2xl  md:text-3xl">How it works</h2>
         <p className="text-center text-xs md:text-base">
-          Take a glimpse below to understand how our platform generally works
+           Glimpse below to understand how our platform generally works
         </p>
       </div>
       <div className="mt-10 w-full flex flex-col md:flex-row items-center gap-6 md:justify-between lg:justify-evenly ">
